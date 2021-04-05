dfce1039fb5af7f5082a3b3061eb85730e016f13
Julia-Liou <80867580+Julia-Liou@users.noreply.github.com>
2021-04-06 00:07:12 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
-123
+alert("HI! welcome to my page!");
+function alertfunction(){
+ alert("Are you ready to know my childhood?");
+}
+function alertfunction(){
+ alert("Are you ready to know my club position?");
+}
+function alertfunction(){
+ alert("Next is to introduce my interests!");
+}
